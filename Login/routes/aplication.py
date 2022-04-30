b88d3d3d85824cf83a50012154432269951b89fb
alexanderRMC <alexander.murillo@ucsp.edu.pe>
2022-04-29 19:01:12 -0500

    realizado el html con css de login

--- Login/routes/aplication.py
+++ Login/routes/aplication.py
@@ -1,7 +1,20 @@
 from flask import Flask,render_template, request
+##from flask_login import LoginManager
+
+class User:
+    def __init__(self,id,username,password):
+        self.id = id
+        self.username = username
+        self.password = password
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+users = []
+users.append(User(id = 1, username='Alex', password='password'))
+users.append(User(id = 2, username='Alan', password='secret'))
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route('/login')
+def login():
+    return render_template('login.html')
